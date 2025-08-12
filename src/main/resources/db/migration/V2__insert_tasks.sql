d5081309d2182a0080519ec3b8c6df1e41bda59a
edvxndxvi <edvandavi1@hotmail.com>
2025-08-11 22:10:04 -0300

    feat: creating and fixing migrations with table inserts

---
+++ src/main/resources/db/migration/V2__insert_tasks.sql
@@ -0,0 +1,8 @@
+INSERT INTO task (title, description, score, status)
+VALUES ('Estudar Java', 'Praticar programação orientada a objetos', 60, 0);
+
+INSERT INTO task (title, description, score, status)
+VALUES ('Criar API REST', 'Desenvolver API com Spring Boot', 30, 40);
+
+INSERT INTO task (title, description, score, status)
+VALUES ('Revisar Banco de Dados', 'Ajustar índices e otimizar queries', 50, 90);
